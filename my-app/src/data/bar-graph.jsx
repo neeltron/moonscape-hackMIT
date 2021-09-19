c767be032b1ceb79dadcea605c28bbe674a90f00
Rehan <Abusayid693@gmail.com>
2021-09-19 17:39:11 +0530

    Update bar-graph.jsx

--- my-app/src/data/bar-graph.jsx
+++ my-app/src/data/bar-graph.jsx
@@ -1,102 +1,103 @@
 
 export const mdata1 = [
     {
-      country: "USA",
-      hydro: 1,
-      oil: 1.1,
+      Week: "1",
+      sleep: 1,
+      deepsleep: 1.1,
     },
     {
-      country: "China",
-      hydro: 1,
-      oil: 0.6,
+      Week: "2",
+      sleep: 1,
+      deepsleep: 1.1,
     },
     {
-      country: "Russia",
-      hydro: 1,
-      oil: 0.2,
+      Week: "3",
+      sleep: 1,
+      deepsleep: 1.1,
     },
     {
-      country: "Japan",
-      hydro: 0.1,
-      oil: 1.6,
+      Week: "4",
+      sleep: 1,
+      deepsleep: 1.1,
     },
     {
-      country: "India",
-      hydro: 0,
-      oil: 0.8,
+      Week: "5",
+      sleep: 1,
+      deepsleep: 1.1,
     },
     {
-      country: "Germany",
-      hydro: 1,
-      oil: 1.6,
+      Week: "6",
+      sleep: 1,
+      deepsleep: 1.1,
+    },
+  ];
+
+
+
+export const mdata2 = [
+    {
+      Week: "1",
+      sleep: 1,
+      deepsleep: 1.1,
+    },
+    {
+      Week: "2",
+      sleep: 1,
+      deepsleep: 1.1,
+    },
+    {
+      Week: "3",
+      sleep: 1,
+      deepsleep: 1.1,
+    },
+    {
+      Week: "4",
+      sleep: 1,
+      deepsleep: 1.1,
+    },
+    {
+      Week: "5",
+      sleep: 1,
+      deepsleep: 1.1,
+    },
+    {
+      Week: "6",
+      sleep: 1,
+      deepsleep: 1.1,
     },
   ];
 
 
 
-  export const mdata2 = [
+export const mdata3 = [
     {
-      country: "USA",
-      hydro: 1,
-      oil: 0,
+      Week: "1",
+      sleep: 1,
+      deepsleep: 1.1,
     },
     {
-      country: "China",
-      hydro: 0.3,
-      oil: 0.1,
+      Week: "2",
+      sleep: 1,
+      deepsleep: 1.1,
     },
     {
-      country: "Russia",
-      hydro: 1,
-      oil: 0.2,
+      Week: "3",
+      sleep: 1,
+      deepsleep: 1.1,
     },
     {
-      country: "Japan",
-      hydro: 0.7,
-      oil: 1.2,
+      Week: "4",
+      sleep: 1,
+      deepsleep: 1.1,
     },
     {
-      country: "India",
-      hydro: 0.55,
-      oil: 0.8,
+      Week: "5",
+      sleep: 1,
+      deepsleep: 1.1,
     },
     {
-      country: "Germany",
-      hydro: 1.3,
-      oil: 1.6,
-    },
-  ];
-
-
-  export const mdata3 = [
-    {
-      country: "USA",
-      hydro: 0.67,
-      oil: 0.23,
-    },
-    {
-      country: "China",
-      hydro: 0.9,
-      oil: 0.3,
-    },
-    {
-      country: "Russia",
-      hydro: 1.4,
-      oil: 0.9,
-    },
-    {
-      country: "Japan",
-      hydro: 0.6,
-      oil: 1.9,
-    },
-    {
-      country: "India",
-      hydro: 1.55,
-      oil: 0.2,
-    },
-    {
-      country: "Germany",
-      hydro: 0.3,
-      oil: 0.6,
+      Week: "6",
+      sleep: 1,
+      deepsleep: 1.1,
     },
   ];
